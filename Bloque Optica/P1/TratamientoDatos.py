e7a4cd1e83c9b767b8f9b3f3776b8a0f753d6d34
Lucoero <lucasnhp227@gmail.com>
2024-11-23 17:40:29 +0100

    update tecnicas

--- Bloque Optica/P1/TratamientoDatos.py
+++ Bloque Optica/P1/TratamientoDatos.py
@@ -4,6 +4,10 @@ Calculo errores P1
 """
 import numpy as np
 import matplotlib.pyplot as plt
+import os as os
+import Herramientas as herr
+
+plt.close("all")
 """
 Parte A: Determinacion del indice de refraccion de una lamina
 """
@@ -23,3 +27,25 @@ errN = n * (relD + relDrel)
 """
 Parte B
 """
+# Coloco el directorio donde esta el script
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+data = herr.TTD("Prisma/data.txt")
+epsi = data[0]
+delta = data[1]
+
+fig,ax = herr.BasicCanvas(r"Angulo de desviación($\delta$) respecto al incidente ($\epsilon$)", r"$\epsilon$ (grados)", r"$\delta$ (grados)")
+
+ax.errorbar(epsi,delta,np.ones(len(epsi)) * 0.5, np.ones(len(epsi)) * 0.5, fmt = "o", color = "black", elinewidth = 0.7, capsize = 3)
+fig.show()
+
+deltaMin = np.min(delta)
+deltaMin = deltaMin * np.pi/180 # Radianes
+errDeltaMin = 0.5 * np.pi/180
+alpha = 60 * np.pi/180
+
+errNb = 1/2 * 1/np.sin(alpha/2)*np.cos((deltaMin + alpha)/2)*errDeltaMin
+nB = np.sin((deltaMin + alpha)/2)/np.sin(alpha/2)
+print("Indice de refraccion:", nB, "+-", errNb)
