ffbb8442ce468ef442f2f812bcd5f3da8cf53f91
Lucoero <lucasnhp227@gmail.com>
2024-12-07 11:06:09 +0100

    Más analisis de datos

--- Experimento a presentar/SternGerlach/Codigo/TresPol.py
+++ Experimento a presentar/SternGerlach/Codigo/TresPol.py
@@ -35,6 +35,17 @@ I60 = PasoAIntensidad(V60)
 errA = np.ones(len(A0))
 errI = np.ones(len(V0)) * 0.01 # MicroAmp
 
+I0Sin = PasoAIntensidad(12.15)
+I30Sin = PasoAIntensidad(10.6)
+I60Sin = PasoAIntensidad(7)
+
+I0N = I0/I0Sin
+I30N = I30/I0Sin
+I60N = I60/I60Sin
+
+errI0N = np.ones(len(I0N)) * np.sqrt((errI/I0N)**2 + (errI/I0Sin)**2) * I0N
+errI30N = np.ones(len(I0N)) * np.sqrt((errI/I30N)**2 + (errI/I30Sin)**2) *I30N 
+errI60N = np.ones(len(I0N)) * np.sqrt((errI/I60N)**2 + (errI/I60Sin)**2) * I60N
 #%% Plots
 
 f1,ax1 = herr.BasicCanvas(r"$\alpha = 0 ^\circ$", r"$\alpha$ (grados)", "I (mA)")
@@ -53,5 +64,34 @@ f1.show()
 f2.show()
 f3.show()
 
+fig4,ax4 = herr.BasicCanvas("Superposición de los tres ángulos",r"$\alpha$ (grados)", "I (mA)")
+ax4.scatter(A0,I0, s = 8, label = r"$\alpha = 0$")
+ax4.scatter(A30,I30, s = 8, label = r"$\alpha = 30^\circ$")
+ax4.scatter(A60,I60, s = 8, label = r"$\alpha = 60^\circ$")
+ax4.legend()
+fig4.show()
 
 
+f5,ax5 = herr.BasicCanvas(r"$\alpha = 0 ^\circ$ (normalizado)", r"$\alpha$ (grados)", "I/IP1")
+f6,ax6 = herr.BasicCanvas(r"$\alpha = 30 ^\circ$ (normalizado)", r"$\alpha$ (grados)", "I/IP1 (mA)")
+f7,ax7 = herr.BasicCanvas(r"$\alpha = 60 ^\circ$ (normalizado)", r"$\alpha$ (grados)", "I/IP1 (mA)")
+f8,ax8 = herr.BasicCanvas("Superposición de los tres ángulos (normalizado)",r"$\alpha$ (grados)", "I/IP1 (mA)")
+
+ax5.scatter(A0,I0N, s = 8)
+ax6.scatter(A30,I30N, s = 8)
+ax7.scatter(A60,I60N, s = 8)
+
+ax8.scatter(A0,I0N, s = 8, label = r"$\alpha = 0$")
+ax8.scatter(A30,I30N, s = 8, label = r"$\alpha = 30^\circ$")
+ax8.scatter(A60,I60N, s = 8, label = r"$\alpha = 60^\circ$")
+
+ax5.errorbar(A0,I0N,errI0N, fmt = "none", capsize = 5) # Quito los errores de Alfa porque solo emborronan
+ax6.errorbar(A30,I30N,errI30N, fmt = "none", capsize = 5)
+ax7.errorbar(A60,I60N,errI60N, fmt = "none", capsize = 5)
+
+ax8.legend()
+
+f5.show()
+f6.show()
+f7.show()
+f8.show()
